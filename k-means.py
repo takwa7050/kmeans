2c22564b849b17e91cc3b0c19a47e132c57c30a9
Ahmed SAMI MOHAMED <asyn.nile@gmail.com>
2016-11-25 17:49:12 +0100

    Cleaning up the code

--- k-means.py
+++ k-means.py
@@ -10,43 +10,44 @@ import math
 
 class KMeans(object):
 
-	def __init__(self, numberOfClusters = 3):
+	def __init__(self, numberOfClusters = 3, maxIterations = 1000):
 		self.numberOfClusters = numberOfClusters
-		self.max_iterations = 1000
 		self.data = self.__readData()
 		self.features = self.data.keys()
+		self.maxIterations = maxIterations
 
 	def run(self):
 		k = self.numberOfClusters
 		centroids = dict((c,[c]) for c in self.features[:k])
 		centroids[self.features[k-1]] += self.features[k:]
-		for i in xrange(self.max_iterations):
-		    new_centroids = self.__assignCentroids(centroids)
-		    new_centroids = self.__updateCentroids(new_centroids)
-		    if centroids == new_centroids:
+		for i in xrange(self.maxIterations):
+		    newCentroids = self.__assignCentroids(centroids)
+		    newCentroids = self.__updateCentroids(newCentroids)
+		    if centroids == newCentroids:
 		        break
 		    else:
-		        centroids = new_centroids
+		        centroids = newCentroids
+		
 		self.__printResult(centroids)
 
 	def __assignCentroids(self, centroids):
-		new_centroids = defaultdict(list)
+		newCentroids = defaultdict(list)
 		for cx in centroids:
 		    for x in centroids[cx]:
 		        best = min(centroids, key=lambda c: self.__calculateDistance(x,c))
-		        new_centroids[best] += [x]
-		return new_centroids
+		        newCentroids[best] += [x]
+		return newCentroids
 
 	def __updateCentroids(self, centroids):
-		new_centroids = {}
+		newCentroids = {}
 		for c in centroids:
-		    new_centroids[self.__mean(centroids[c])] = centroids[c]
-		return new_centroids
+		    newCentroids[self.__mean(centroids[c])] = centroids[c]
+		return newCentroids
 
 	def __calculateDistance(self, feature1, feature2):
-		a = np.array
-		d = a(feature1)-a(feature2)
-		return np.sqrt(np.dot(d, d))
+		features = np.array
+		distance = features(feature1)-features(feature2)
+		return np.sqrt(np.dot(distance, distance))
 
 	def __mean(self, features):
 	    return tuple(np.mean(features, axis=0))
@@ -57,16 +58,15 @@ class KMeans(object):
 	        count[x] += 1
 	    return dict(count)
 
-	def __printResult(self, clusters):
-		for c in clusters:
-		    print self.__counter([self.data[x] for x in clusters[c]])
-
 	def __readData(self):
 		data = [l.strip() for l in open('iris.data') if l.strip()]
 		features = [tuple(map(float, x.split(',')[:-1])) for x in data]
 		labels = [x.split(',')[-1] for x in data]
 		return dict(zip(features, labels))
 
+	def __printResult(self, clusters):
+		for c in clusters:
+		    print self.__counter([self.data[x] for x in clusters[c]])
 
 if __name__ == "__main__":
     KMeans().run()
